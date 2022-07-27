ac051dc3e1b90424ef1998db9aa742de3e9dfe4f
Deepak Gurjar <78991392+deepak-gurjar07@users.noreply.github.com>
2022-07-28 04:58:13 +0530

    Create prgm1.cpp

---
+++ C++ Basic/prgm1.cpp
@@ -0,0 +1,12 @@
+
+#include<iostream>
+using namespace std;
+int main (){
+    int num1, num2;
+    cout<<"Enter 1st number: ";
+    cin>>num1;
+    cout<<"Enter 2nd number: ";
+    cin>>num2;
+    cout<<"The sum of two numbers ("<<num1<<","<<num2<<") is: "<<num1+num2;    
+    return 0;
+}
